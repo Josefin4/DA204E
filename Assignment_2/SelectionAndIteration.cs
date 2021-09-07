e89e89b3ef60672ca395091c320eb1318cc8be31
Josefin Salomaa <josefinsalom@gmail.com>
2021-09-07 20:59:19 +0200

    assignment2

---
+++ Assignment_2/SelectionAndIteration.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_2
+{
+	class SelectionAndIteration
+	{
+		public SelectionAndIteration()
+		{
+			Console.WriteLine();
+			Console.WriteLine("My name is Josefin and I am a student of the VT 21 semester!");
+			Console.WriteLine();
+
+			ShowStringLength();
+			Console.WriteLine();
+
+			MakeMyDay();
+			Console.WriteLine();
+
+			SumNumbers();
+		}
+
+		public void ShowStringLength()
+		{
+			string text = string.Empty;
+			bool done = false;
+
+			Console.WriteLine("Let me calculate the length of strings for you!");
+			do
+			{
+
+				Console.WriteLine("Give me a text or any length, or press Enter to exit!\n");
+				text = Console.ReadLine();
+
+				int length = text.Length;
+				if (length == 0)
+				{
+					done = true;
+				}
+				else
+				{
+					text = text.ToUpper();
+					Console.Write(text + "\n");
+					Console.Write("Number of chars = " + length + "\n" + "\n");
+				}
+			}
+			while (!done);
+		}
+
+		public void MakeMyDay()
+		{
+			Console.Write("I can predict your day for you. ");
+			Console.Write("Give a number between 1 and 7: ");
+			int day = int.Parse(Console.ReadLine());
+			string fortune = string.Empty;
+
+			switch (day)
+			{
+				case 1:
+					fortune = "Monday: Keep calm! You can fall apart!";
+					break;
+				case 2:
+				case 3:
+					fortune = "Tuesday and Wednesday break your heart!";
+					break;
+				case 4:
+					fortune = "Thursday, Uuush, still one day to Friday!";
+					break;
+				case 5:
+					fortune = "It's Friday! You are in love!";
+					break;
+				case 6:
+					fortune = "Saturday, do nothing and do plenty of it!";
+					break;
+				case 7:
+					fortune = "And Sunday always comes too soon!";
+					break;
+				default:
+					fortune = "Not in a good mode? This is not a valid date!";
+					break;
+			}
+			Console.WriteLine(fortune);
+		}
+
+		private int SumNumbers(int start, int end)
+		{
+			int sum = 0;
+			for (int i = start; i <= end; i++)
+			{
+				sum = sum + i;
+			}
+			return sum;
+		}
+
+		public void SumNumbers()
+		{
+			Console.WriteLine();
+			Console.Write("Give start number: ");
+
+			int startNum = int.Parse(Console.ReadLine());
+			Console.Write("Give end number: ");
+			int endNum = int.Parse(Console.ReadLine());
+			int temp = startNum;
+
+			if (startNum > endNum)
+			{
+				startNum = endNum;
+				endNum = temp;
+			}
+			int sum = SumNumbers(startNum, endNum);
+			Console.WriteLine("The sum between " + startNum + " and " + endNum + " is: " + sum);
+		}
+
+	}
+}
---
+++ Assignment_2/TemperatureConverter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_2
+{
+	class TemperatureConverter
+	{
+		public TemperatureConverter()
+		{
+			Console.Clear();
+			Start();
+		}
+		public void Start()
+		{
+			int choice = -1;
+			while (choice != 0)
+			{
+				ShowMenu();
+				choice = int.Parse(Console.ReadLine());
+			}
+			switch (choice)
+			{
+				case 0:
+					break;
+				case 1:
+					ShowTableCelsiusToFahrenheit();
+					break;
+				case 2:
+					ShowTableFahrenheitToCelsius();
+					break;
+				default:
+					Console.WriteLine("Invalid option, try again!");
+					break;
+			}
+		}
+		public void ShowMenu()
+		{
+		}
+		private void ShowTableCelsiusToFahrenheit()
+		{
+			const int columns = 2;
+			const int max = 100;
+			int p = 0;
+			for (int i = 0; i < max; i += 4)
+			{
+				double result = CelsiusToFahrenheit(i);
+				Console.Write("{0,6:f2} C= {1,6:f2} F ", i, result);
+
+				p++;
+				if ((p % columns == 0) && (p >= columns))
+					Console.WriteLine();
+			}
+		}
+		private double CelsiusToFahrenheit(double celsius)
+		{
+			return 9.0 / 5.0 * celsius + 32.0;
+		}
+		private void ShowTableFahrenheitToCelsius()
+		{
+			const int columns = 2;
+			const int max = 100;
+			int p = 0;
+			for (int i = 0; i < max; i += 4)
+			{
+				double result = FahrenheitToCelsius(i);
+				Console.Write("{0,6:f2} C= {1,6:f2} F ", i, result);
+
+				p++;
+				if ((p % columns == 0) && (p >= columns))
+					Console.WriteLine();
+			}
+		}
+		private double FahrenheitToCelsius(double celsius)
+		{
+			return 9.0 / 5.0 * celsius + 32.0;
+		}
+	}
+}
